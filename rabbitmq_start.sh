7e5167a523f357ae22aeb2268709c16c88ba77aa
Evgeny Erofeev <erofeev@regenergy.ru>
2019-12-07 11:47:17 +0300

    fix

--- rabbitmq_start.sh
+++ rabbitmq_start.sh
@@ -10,6 +10,11 @@ docker run \
     --rm \
     --name=rabbitmq \
     --hostname=$HOSTNAME \
+    --add-host=rabbit-ccsvps:62.109.17.171 \
+    --add-host=rabbit-dcccsast:46.229.223.203 \
+    --add-host=rabbit-dcccsapp:46.229.223.208 \
+    --add-host=rabbit-dclvccsast:195.13.217.92 \
+    --add-host=rabbit-dclvccsapp:195.13.217.93 \
     -p 5672:5672 \
     -p 15672:15672 \
     -v $SCRIPT_ORIGDIR/data:/var/lib/rabbitmq \
